05f61b94939e9ad744b4875f38f34e3a5ef2dc9d
rainkinz <brendan.grainger@gmail.com>
2021-02-17 15:08:15 +1300

    updated build

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-docker build -t rainkinz/s3rver:1.0 .
+set -e
 
-#docker push rainkinz/s3rver
+VERSION="1.0"
+
+docker build -t rainkinz/s3rver:$VERSION .
+
+docker push rainkinz/s3rver:$VERSION
+
+docker tag rainkinz/s3rver:$VERSION  rainkinz:latest
+docker push rainkinz/s3rver:latest
